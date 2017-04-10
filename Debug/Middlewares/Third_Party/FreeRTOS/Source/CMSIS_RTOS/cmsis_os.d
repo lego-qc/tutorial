29ee018c81983bc927e8adc211124299ce9d99c3
marton bognar <martonbognar@sch.bme.hu>
2017-04-10 18:56:14 +0200

    Initial commit

---
+++ Debug/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.d
@@ -0,0 +1,62 @@
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h \
+ C:/Users/bogna/workspace/qc/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/bogna/workspace/qc/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/bogna/workspace/qc/Inc/FreeRTOSConfig.h \
+ C:/Users/bogna/workspace/qc/Inc/main.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+
+../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h:
+
+C:/Users/bogna/workspace/qc/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/bogna/workspace/qc/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/bogna/workspace/qc/Inc/FreeRTOSConfig.h:
+
+C:/Users/bogna/workspace/qc/Inc/main.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+C:/Users/bogna/workspace/qc/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
